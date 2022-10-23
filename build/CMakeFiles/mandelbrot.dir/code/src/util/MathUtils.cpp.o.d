8b226f392ad8a48f563aedc86bb66fd28e9ef3e7
christopherrons <christopherheron09@gmail.com>
2022-10-23 15:18:09 +0200

    Performance fixes

---
+++ build/CMakeFiles/mandelbrot.dir/code/src/util/MathUtils.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/mandelbrot.dir/code/src/util/MathUtils.cpp.o: \
+ /home/christopher/dev/mandelbrot/code/src/util/MathUtils.cpp \
+ /usr/include/stdc-predef.h \
+ /home/christopher/dev/mandelbrot/code/src/util/../../header/utils/MathUtils.h \
+ /usr/include/SFML/Config.hpp
---
+++ cmake-build-debug/CMakeFiles/mandelbrot.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/christopher/dev/mandelbrot/code/main.cpp" "CMakeFiles/mandelbrot.dir/code/main.cpp.o" "gcc" "CMakeFiles/mandelbrot.dir/code/main.cpp.o.d"
+  "/home/christopher/dev/mandelbrot/code/src/gui/WindowHandler.cpp" "CMakeFiles/mandelbrot.dir/code/src/gui/WindowHandler.cpp.o" "gcc" "CMakeFiles/mandelbrot.dir/code/src/gui/WindowHandler.cpp.o.d"
+  "/home/christopher/dev/mandelbrot/code/src/mandelbrot/MandelbrotCalculator.cpp" "CMakeFiles/mandelbrot.dir/code/src/mandelbrot/MandelbrotCalculator.cpp.o" "gcc" "CMakeFiles/mandelbrot.dir/code/src/mandelbrot/MandelbrotCalculator.cpp.o.d"
+  "/home/christopher/dev/mandelbrot/code/src/mandelbrot/MandelbrotResult.cpp" "CMakeFiles/mandelbrot.dir/code/src/mandelbrot/MandelbrotResult.cpp.o" "gcc" "CMakeFiles/mandelbrot.dir/code/src/mandelbrot/MandelbrotResult.cpp.o.d"
+  "/home/christopher/dev/mandelbrot/code/src/util/ConfigUtils.cpp" "CMakeFiles/mandelbrot.dir/code/src/util/ConfigUtils.cpp.o" "gcc" "CMakeFiles/mandelbrot.dir/code/src/util/ConfigUtils.cpp.o.d"
+  "/home/christopher/dev/mandelbrot/code/src/util/MathUtils.cpp" "CMakeFiles/mandelbrot.dir/code/src/util/MathUtils.cpp.o" "gcc" "CMakeFiles/mandelbrot.dir/code/src/util/MathUtils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
